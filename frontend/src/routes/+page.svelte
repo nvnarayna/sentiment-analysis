3f12c29af08a11540121908fe3f7c68d689fa0d8
nvnarayana <98206357+nvnarayna@users.noreply.github.com>
2023-09-06 08:07:40 +0000

    build

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,8 +3,9 @@
     import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
     import Explaination from "$lib/Explaination.svelte";
     let tabSet: number = 0;
-    if ($:tabSet ==2){
-        window.open("")
+    let redirect_git=()=>{
+        window.open("https://github.com/nvnarayna/sentiment-analysis")
+        tabSet=0
     }
 </script>
 
@@ -12,7 +13,7 @@
 <TabGroup justify="justify-center">
 	<Tab bind:group={tabSet} name="tab1" value={0}>Try</Tab>
 	<Tab bind:group={tabSet} name="tab2" value={1}>explaination</Tab>
-	<Tab bind:group={tabSet} name="tab3" value={2}>Source Github</Tab>
+	<Tab on:click={redirect_git} bind:group={tabSet} name="tab3" value={2}>Source Github</Tab>
 	<!-- Tab Panels --->
 	<svelte:fragment slot="panel">
 		{#if tabSet === 0}
